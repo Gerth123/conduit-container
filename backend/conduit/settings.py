b4d4c616abb5624d657d589c9b5cc4964f51670b
Robin Gerth <robing@abc-design.de>
2026-08-26 12:20:14 +0200

    Switch database backend from SQLite to PostgreSQL

--- backend/conduit/settings.py
+++ backend/conduit/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'conduit'),
+        'USER': os.environ.get('POSTGRES_USER', 'conduit'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'conduit'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
